1e29efc9ebdf3ec5e4dc451f4ea44d8b2045b88c
Jivago Alves <jivagoalves@gmail.com>
2012-03-05 23:30:12 -0500

    Add spec and implementation for part 2

--- lib/homework-1-ruby-calisthenics/solution_part_2.rb
+++ lib/homework-1-ruby-calisthenics/solution_part_2.rb
@@ -1,5 +1,28 @@
 class WrongNumberOfPlayersError < StandardError ; end
+class NoSuchStrategyError < StandardError ; end
+
+RULES = {"R" => "S", "P" => "R", "S" => "P"}
+
+def check_errors!(game)
+  raise WrongNumberOfPlayersError unless game.length == 2
+  raise NoSuchStrategyError unless ["P","R","S"].include?(game.first.last.upcase)
+  raise NoSuchStrategyError unless ["P","R","S"].include?(game.last.last.upcase)
+end
 
 def rps_game_winner(game)
-  raise WrongNumberOfPlayersError unless game.length == 2
+  check_errors!(game)
+
+  first_player = game.first
+  first_player_strategy = first_player.last
+  second_player = game.last
+  second_player_strategy = second_player.last
+
+  return first_player unless first_player_strategy != second_player_strategy
+  return first_player if RULES[first_player_strategy] == second_player_strategy
+  second_player
+end
+
+def rps_tournament_winner(tournament)
+  return tournament if tournament.first.is_a? String
+  return rps_game_winner([ rps_tournament_winner(tournament.first), rps_tournament_winner(tournament.last) ])
 end
--- spec/homework-1-ruby-calisthenics/solution_part_2_spec.rb
+++ spec/homework-1-ruby-calisthenics/solution_part_2_spec.rb
@@ -3,17 +3,63 @@ require 'spec_helper'
 describe "rps_game_winner" do
   context "when the number of players is not equal to 2" do
     it "should raise WrongNumberOfPlayersError" do
-      one_player = [[ "Armando", "P" ]]
       expect {
-        rps_game_winner(one_player)
+        rps_game_winner([ ["P1","R"] ])
       }.to raise_error WrongNumberOfPlayersError
 
-      three_players = one_player
-      three_players << ["Ji", "S"]
-      three_players << ["Mama", "R"]
       expect {
-        rps_game_winner(three_players)
+        rps_game_winner([ ["P1","R"], ["P2","S"], ["P3","P"] ])
       }.to raise_error WrongNumberOfPlayersError
+    end
+  end
+
+  context "when player's strategy is something other than 'R', 'P' or 'S'" do
+    it "should raise NoSuchStrategyError" do
+      ["P","R","S","p","r","s"].each do |strategy|
+        expect {
+          rps_game_winner([ ["P1","A"], ["P2", strategy] ])
+        }.to raise_error NoSuchStrategyError
+
+        expect {
+          rps_game_winner([ ["P1",strategy], ["P2", "A"] ])
+        }.to raise_error NoSuchStrategyError
+      end
+    end
+  end
+
+  context "when both players strategy are valid" do
+    let(:player_rock)     { ["PlayerRock", "R"] }
+    let(:player_paper)    { ["PlayerPaper", "P"] }
+    let(:player_scissors) { ["PlayerScissors", "S"] }
+
+    context "and both players have the same strategy" do
+      it "should return the first player as winner" do
+        ["P","R","S","p","r","s"].each do |strategy|
+          first_player = ["P1",strategy]
+	  rps_game_winner([ first_player ,["P2", strategy] ]).should be_equal(first_player)
+        end
+      end
+    end
+
+    context "and a player choose rock" do
+      it "should beats scissors" do
+        rps_game_winner([ player_rock, player_scissors ]).should be_equal(player_rock)
+        rps_game_winner([ player_scissors, player_rock ]).should be_equal(player_rock)
+      end
+    end
+
+    context "and a player choose paper" do
+      it "should beats rock" do
+        rps_game_winner([ player_rock, player_paper ]).should be_equal(player_paper)
+        rps_game_winner([ player_paper, player_rock ]).should be_equal(player_paper)
+      end
+    end
+
+    context "and a player choose scissors" do
+      it "should beats paper" do
+        rps_game_winner([ player_scissors, player_paper ]).should be_equal(player_scissors)
+        rps_game_winner([ player_paper, player_scissors ]).should be_equal(player_scissors)
+      end
     end
   end
 end
